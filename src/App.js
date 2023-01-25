c5f8a18082318d88a16d029905acf3fd51e5a720
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-25 18:25:30 +0900

    [2].jsx AND 연산자를 사용한 조건부 랜더링

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import { Fragment } from "react"; 
-const name ='리엑트';
+const name ='리웩트';
 
 function App() {
   return (    
-    <>{/* Fragment -> 단축 <></>  : 상위 'import { Fragment } from "react"' 추가 필요 */}
-    {/* if 문 x -> 삼항 연산자 o */}
-    {name === '리엑트' ? (
-      <h1>리엑트 입니다.</h1>
-    ):(
-      <h1>리엑트가 아닙니다.</h1>
-    )}
-      
-      <h2>잘 작동 하니?</h2>
+    <>
+    {name === '리엑트' ? <h1>리엑트 입니다.</h1>:<h1>리엑트가 아닙니다.</h1>}
     </>
   );
 }

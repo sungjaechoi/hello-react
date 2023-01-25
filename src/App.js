3d32e747d432cfb18b7a8db604605d36aa635bbe
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-25 17:50:26 +0900

    [2].jsx 자바스크립트 표현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Fragment } from "react"; 
+const name ='리엑트';
 
 function App() {
-  return (
-    
-    <Fragment>{/* Fragment -> 단축 <></>  : 상위 'import { Fragment } from "react"' 추가 필요 */}
-      <h1>리엑트 안녕!</h1>
+  return (    
+    <>{/* Fragment -> 단축 <></>  : 상위 'import { Fragment } from "react"' 추가 필요 */}
+      <h1>{name}안녕!</h1>
       <h2>잘 작동 하니?</h2>
-    </Fragment>
+    </>
   );
 }
 

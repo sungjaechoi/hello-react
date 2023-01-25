97e8476b162c79c2523562621fda8e4793209512
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-25 18:03:41 +0900

    [2].jsx if문 대신 조건부 연산자

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ const name ='리엑트';
 function App() {
   return (    
     <>{/* Fragment -> 단축 <></>  : 상위 'import { Fragment } from "react"' 추가 필요 */}
-      <h1>{name}안녕!</h1>
+    {/* if 문 x -> 삼항 연산자 o */}
+    {name === '리엑트' ? (
+      <h1>리엑트 입니다.</h1>
+    ):(
+      <h1>리엑트가 아닙니다.</h1>
+    )}
+      
       <h2>잘 작동 하니?</h2>
     </>
   );

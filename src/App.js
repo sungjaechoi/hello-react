bc13fa5655c1f0f070587bd7bc1884458528b1d2
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-25 16:21:09 +0900

    [2].jsx 감싸인 요소

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react"; 
+
+function App() {
+  return (
+    
+    <Fragment>{/* Fragment -> 단축 <></>  : 상위 'import { Fragment } from "react"' 추가 필요 */}
+      <h1>리엑트 안녕!</h1>
+      <h2>잘 작동 하니?</h2>
+    </Fragment>
+  );
+}
+
+export default App;

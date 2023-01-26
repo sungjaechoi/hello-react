35ec1fccc570d62e15cc4551f85646bdd832d3ed
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-26 09:18:09 +0900

    [2].jsx 인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import'./App.css';
 
 function App() {
-  const name = undefined;
+  const name = '리엑트';
+  const style = {
+    // background-color는 backgroundColor와 같이 '-'가 사라지고 카멜 표시법으로 작성
+    backgroundColor:'black',
+    color:'aqua',
+    fontSize:'48px', //font-size
+    fontWeight:'bold',//font-weight
+    padding:16 // 단위를 생략 하면 px로 지정
+  }
   return (
-    <div>{name || '리엑트'}</div>
+    <div style ={style}>{name}</div>
   )
 }
 

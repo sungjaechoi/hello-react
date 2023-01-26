b4e89d47321556c5d21c3fdc381b1f78eae321ff
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-26 09:28:51 +0900

    [2].jsx 인라인 스타일링 - jsx 내부 직접 선언 방식

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import'./App.css';
 
 function App() {
   const name = '리엑트';
-  const style = {
-    // background-color는 backgroundColor와 같이 '-'가 사라지고 카멜 표시법으로 작성
-    backgroundColor:'black',
-    color:'aqua',
-    fontSize:'48px', //font-size
-    fontWeight:'bold',//font-weight
-    padding:16 // 단위를 생략 하면 px로 지정
-  }
   return (
-    <div style ={style}>{name}</div>
+    // 소괄호로 "style ={}" 내부에서 한번더 감싸 준다 -> "style ={{}}"
+    <div style ={{
+      // background-color는 backgroundColor와 같이 '-'가 사라지고 카멜 표시법으로 작성
+      backgroundColor:'black',
+      color:'aqua',
+      fontSize:'48px', //font-size
+      fontWeight:'bold',//font-weight
+      padding:16 // 단위를 생략 하면 px로 지정
+    }}>
+      {name}
+    </div>
   )
 }
 

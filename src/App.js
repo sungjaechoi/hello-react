12fc50a5755515cb0cc384778a5d8107111e1198
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-28 16:27:48 +0900

    [3].클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import'./App.css';
+import { Component } from "react";
 
-function App() {
-  const name = '리엑트';
-  return (
-    <>
-      <div className='react'>{name}</div>
-      {/* <input> -> Parsing error: Unterminated JSX contents. (구문오류)*/}
-      {/* <input></input> -> 정상적 출력 */}
-      <input/>{/*태그사이의 별도의 내용이 들어가지 않는 경우 사용 가능(self-closing)*/}
-    </>
-  );
+class App extends Component {
+  render(){
+    const name = 'react';
+    return<div className='react'>{name}</div>
+  }
 }
 
 export default App;

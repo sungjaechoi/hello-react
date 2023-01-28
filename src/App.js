27b2e40a92983fec6b398e236bfaa010e9b4e173
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-28 15:24:50 +0900

    [2].jsx 꼭 닫아야 하는 태그

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import'./App.css';
 
 function App() {
   const name = '리엑트';
-  return <div className='react'>{name}</div>
+  return (
+    <>
+      <div className='react'>{name}</div>
+      {/* <input> -> Parsing error: Unterminated JSX contents. (구문오류)*/}
+      {/* <input></input> -> 정상적 출력 */}
+      <input/>{/*태그사이의 별도의 내용이 들어가지 않는 경우 사용 가능(self-closing)*/}
+    </>
+  );
 }
 
 export default App;

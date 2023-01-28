fc15720ed370df175b94c046a829c05eb2437cbe
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-28 19:33:34 +0900

    [3].첫 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { Component } from "react";
+import MyComponent from './MyComponent';
 
-class App extends Component {
-  render(){
-    const name = 'react';
-    return<div className='react'>{name}</div>
-  }
-}
+const App = () => {
+  return <MyComponent/>;
+};
 
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,6 @@
+const MyComponent = ()=>{
+  return <div>나의 새롭고 멋진 컴포넌트</div>
+};
+
+export default MyComponent;
+//다른 파일에서 이파일을 import 할때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정

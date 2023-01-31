b77da0083da7662c66196b43bf24df361fce6c97
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-01-31 19:16:05 +0900

    [3].props - 1.jsx 내부에서 props 렌더링

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,5 @@
-const MyComponent = ()=>{
-  return <div>나의 새롭고 멋진 컴포넌트</div>
+const MyComponent = props =>{
+  return <div>안녕하세요, 제이름은{props.name}입니다.</div>
 };
 
 export default MyComponent;
-//다른 파일에서 이파일을 import 할때, 위에서 선언한 MyComponent 클래스를 불러오도록 설정

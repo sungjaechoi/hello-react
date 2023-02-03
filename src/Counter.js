0cf6440a5cabc8440d7b913d983e3e2e3685fb61
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-02-04 01:03:24 +0900

    [3].state - 1.클래스형 컴포넌트의 state - this.setSate에 객체 대신 함수 인자 전달하기

--- src/Counter.js
+++ src/Counter.js
@@ -6,16 +6,21 @@ class Counter extends Component {
     fixedNumber: 0,
   };
   render() {
-    const { number, fixedNumber } = this.state; //state를 조회할 때는 this.state로 조회합니다.
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
         <h2>바뀌지 않는 값: {fixedNumber}</h2>
         <button
-          // onClick을 통해 버튼이 클릭되었을때 호출할수 있는 함수를 지정 합니다.
           onClick={() => {
-            //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-            this.setState({ number: number + 1 });
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            this.setState((prevState) => ({ 
+              number: prevState.number + 1 
+            }));
           }}
         >
           +1

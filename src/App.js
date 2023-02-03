66ab71d940195e685aaa3a8ffcd91d80d04033be
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-02-03 23:54:32 +0900

    [3].state - 1.클래스형 컴포넌트의 state - Counter 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import MyComponent from './MyComponent';
+import Counter from "./Counter";
 
 const App = () => {
-  return <MyComponent name='React' favoriteNumber={1}>리엑트</MyComponent>;
+  return <Counter />;
 };
 
 export default App;

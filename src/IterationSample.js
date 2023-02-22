3b89662456e20cb0107b085e45fc63b75cd3f47f
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-02-22 11:28:24 +0900

    [6].컴포넌트 반복 - 1. map() 적용

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,11 +1,8 @@
 const IterationSample = () => {
+  const names = ['눈사람', '얼음', '눈', '바람'];
+  const nameList = names.map(name => <li>{name}</li>)
   return(
-    <ul>
-      <li>눈사람</li>
-      <li>얼음</li>
-      <li>눈</li>
-      <li>바람</li>
-    </ul>
+    <ul>{nameList}</ul>
   );
 };
 

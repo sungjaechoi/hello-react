aedccdc3873d74e015d43ebb10656e55e964dba8
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-02-27 18:43:09 +0900

    [6].컴포넌트 반복 - 3. 응용 - 데이터 추가 기능 구현

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -8,10 +8,28 @@ const IterationSample = () => {
     { id: 4, text: "바람" },
   ]);
   const [inputText, setInputText] = useState("");
-  const [nexId, setNextId] = useState(5); //새로운 항목을 추가할 때 사용할 id
+  const [nextId, setNextId] = useState(5); //새로운 항목을 추가할 때 사용할 id
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, //nexId 값에 1을 더해준다.
+      text: inputText,
+    });
+    setNextId(nextId + 1); //nextId 값에 1을 더해준다
+    console.log(nextId);
+    setNames(nextNames); //names 값을 업데이트한다.
+    setInputText(""); //inputText를 비운다.
+  };
 
   const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
-  return <ul>{namesList}</ul>;
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{namesList}</ul>;
+    </>
+  );
 };
 
 export default IterationSample;

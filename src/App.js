e6ea881e4ac7ccb8adf0240bc0f30f87da38eb6c
sungjaechoi39 <sungjaechoi39@gmail.com>
2023-03-10 17:48:38 +0900

    [7].컴포넌트 라이프 사이클 - 1. 예제 컴포넌트(라이프 사이클 확인)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import IterationSample from "./IterationSample";
+import React, { useState } from "react";
+import LifeCycleSample from "./LifeCycleSample";
 
-const App = () => {
-  return <IterationSample />;
-};
+// 랜덤 색상을 생성합니다
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function App() {
+  const [color, setColor] = useState("#000000");
+  const [visible, setVisible] = useState(true);
+
+  const onClick = () => {
+    setColor(getRandomColor());
+  };
+
+  const onToggle = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <button onClick={onClick}>랜덤 색상</button>
+      <button onClick={onToggle}>삭제</button>
+      {visible && <LifeCycleSample color={color} />}
+    </>
+  );
+}
 
 export default App;

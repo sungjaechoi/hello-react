8d9b26c345f0914e95b4711ee195f9c7ac2d451b
sungjaechoi39 <sungjaechoi39@hivelab.co.kr>
2023-08-24 17:19:16 +0900

    [9].컴포넌트 스타일링 - Css node_modules + classnames labrary

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import styles from './CSSModule.module.css'
+import classnames from 'classnames/bind'
+
+const css = classnames.bind(styles);
 
 const CSSModule = () => {
+
+  //classnames 라이브러리를 사용하지 않고 class를 조건부 사용 시
+  const { warpper, inverted } = styles;
+
+  //classnames 라이브러리 사용 시 state 전달 class 조건부 사용
+  const [divToggle, setDivToggle] = useState(false);
+  const onClick = () => {
+    divToggle ? setDivToggle(false) : setDivToggle(true);
+  };
+
   return (
     // CSSModule 사용시 ClassName에 "className={styles.클래스이름}" 형태로 전달
     // 출력 시 .파일이름_클래스이름_해시값 형태로 노출
@@ -9,11 +23,22 @@ const CSSModule = () => {
     //백틱을 활용 한 방법, 배열을 활용한 빙법
     <>
       {/* 백틱 */}
-      <div className={`${styles.warpper} ${styles.inverted}`}>
+      <div className={`${styles.warpper} ${styles.warpper}`}>
         안녕하세요, 저는 <span className="something">CSS Module!</span>
       </div>
       {/* 배열 */}
       <div className={[styles.warpper, styles.inverted].join(" ")}>
+        안녕하세요, 저는 <span className="something">CSS Module!</span>
+      </div>
+      {/* classnames 사용 x */}
+      <div className={`${warpper} ${inverted ? "" : inverted}`}>
+        안녕하세요, 저는 <span className="something">CSS Module!</span>
+      </div>
+      {/* classnames 라이브러 */}
+      <div
+        className={css("warpper", { inverted: divToggle })}
+        onClick={onClick}
+      >
         안녕하세요, 저는 <span className="something">CSS Module!</span>
       </div>
     </>

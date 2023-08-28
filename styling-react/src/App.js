7ed88ee26cbbaec8b1facffb7553f1ecdff948af
sungjaechoi39 <sungjaechoi39@hivelab.co.kr>
2023-08-28 12:30:30 +0900

    [9].컴포넌트 스타일링 - styled-components(css-in-js)

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,12 +1,17 @@
 import SassComponent from './SassComponent';
+import StyledComponent from "./StyledComponent";
 import CSSModule from './CSSModule';
 
 function App() {
   return (
     <>
-      <SassComponent />
-      <CSSModule/>
+      <SassComponent /> 
+      <CSSModule />
+      <div>
+        <StyledComponent />
+      </div>
     </>
+    
   );
 }
 
